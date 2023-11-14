398fe2da9ee5e2c3048bfb84803311cc303fb431
frank <no>
2023-11-14 14:20:17 +0800

    tidy

--- internal/logic/db/tfa.go
+++ internal/logic/db/tfa.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gogf/gf/v2/database/gdb"
 	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/frame/g"
 	"github.com/mpcsdk/mpcCommon/mpccode"
 )
 
@@ -50,20 +51,19 @@ func (s *sDB) InsertTfaInfo(ctx context.Context, userId string, data *do.Tfa) er
 		return nil
 	}
 
-	_, err = dao.Tfa.Ctx(ctx).Cache(gdb.CacheOption{
-		Duration: s.dbDuration,
+	_, err = g.Model(dao.Tfa.Table()).Ctx(ctx).Cache(gdb.CacheOption{
+		Duration: -1,
 		Name:     dao.Tfa.Table() + userId,
 		Force:    false,
-	}).Data(data).
-		Insert()
+	}).Data(data).Insert()
 
 	return err
 }
 
 // //
 func (s *sDB) UpdateTfaInfo(ctx context.Context, userId string, data *do.Tfa) error {
-	_, err := dao.Tfa.Ctx(ctx).Cache(gdb.CacheOption{
-		Duration: s.dbDuration,
+	_, err := g.Model(dao.Tfa.Table()).Ctx(ctx).Cache(gdb.CacheOption{
+		Duration: -1,
 		Name:     dao.Tfa.Table() + userId,
 		Force:    false,
 	}).Data(data).Where(do.Tfa{
@@ -98,11 +98,11 @@ func (s *sDB) FetchTfaInfo(ctx context.Context, userId string) (*entity.Tfa, err
 		UserId: userId,
 	}
 	var data *entity.Tfa
-	rst, err := dao.Tfa.Ctx(ctx).Cache(gdb.CacheOption{
+	///
+	rst, err := g.Model(dao.Tfa.Table()).Ctx(ctx).Cache(gdb.CacheOption{
 		Duration: s.dbDuration,
 		Name:     dao.Tfa.Table() + userId,
 		Force:    false,
-		// }).Where("user_id", 1).One()
 	}).Where(aggdo).One()
 	if err != nil {
 		err = gerror.Wrap(err, mpccode.ErrDetails(
